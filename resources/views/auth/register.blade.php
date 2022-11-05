63a85204414cbac96e68280840abef80d1a8c5bd
shafi97 <msh.shafiul@gmail.com>
2022-11-05 13:42:39 +0600

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,18 +6,21 @@
     <title>Register</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <link rel="icon" href="../assets/img/icon.ico" type="image/x-icon" />
-
     <script src="{{ asset('backend/js/plugin/webfont/webfont.min.js') }}"></script>
-	<script>
-		WebFont.load({
-			google: {"families":["Open+Sans:300,400,600,700"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"], urls: ['{{ asset("backend/css/fonts.css") }}']},
-			active: function() {
-				sessionStorage.fonts = true;
-			}
-		});
-	</script>
-
+    <script>
+        WebFont.load({
+            google: {
+                "families": ["Open+Sans:300,400,600,700"]
+            },
+            custom: {
+                "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"],
+                urls: ['{{ asset('backend/css/fonts.css') }}']
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+        });
+    </script>
     <!-- CSS Files -->
     <link rel="stylesheet" href="{{ asset('backend/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('backend/css/azzara.min.css') }}">
@@ -27,21 +30,21 @@
     <div class="wrapper wrapper-login">
         <div class="container container-login animated fadeIn">
             <h3 class="text-center">Register</h3>
-            <form method="POST" action="{{route('registerStore')}}">
+            <form method="POST" action="{{ route('registerStore') }}">
                 @csrf
                 <div class="login-form">
                     <div class="form-group">
                         <label for="name" class="placeholder"><b>Name</b></label>
                         <input type="name" name="name" class="form-control" value="{{ old('name') }}" required>
                         @if ($errors->has('name'))
-                        <div class="alert alert-danger">{{ $errors->first('name') }}</div>
+                            <div class="alert alert-danger">{{ $errors->first('name') }}</div>
                         @endif
                     </div>
                     <div class="form-group">
                         <label for="email" class="placeholder"><b>Email</b></label>
                         <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
                         @if ($errors->has('email'))
-                        <div class="alert alert-danger">{{ $errors->first('email') }}</div>
+                            <div class="alert alert-danger">{{ $errors->first('email') }}</div>
                         @endif
                     </div>
 
@@ -49,12 +52,13 @@
                     <div class="form-group">
                         <label for="passwordsignin" class="placeholder"><b>Password</b></label>
                         <div class="position-relative">
-                            <input type="password" name="password" class="form-control" id="passwordsignin" required autocomplete="new-password">
+                            <input type="password" name="password" class="form-control" id="passwordsignin" required
+                                autocomplete="new-password">
                             <div class="show-password">
                                 <i class="flaticon-interface"></i>
                             </div>
                             @if ($errors->has('password'))
-                            <div class="alert alert-danger">{{ $errors->first('password') }}</div>
+                                <div class="alert alert-danger">{{ $errors->first('password') }}</div>
                             @endif
                         </div>
                     </div>
@@ -83,4 +87,5 @@
 </body>
 <script src="{{ asset('backend/js/core/jquery.3.2.1.min.js') }}"></script>
 <script src="{{ asset('backend/js/ready.js') }}"></script>
+
 </html>
