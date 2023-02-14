93bd9a6619f89fee09065516f27a021b5863b433
shafi97 <msh.shafiul@gmail.com>
2023-02-14 21:21:28 +0600

    group

--- resources/views/admin/animal_tag/group/admin.blade.php
+++ resources/views/admin/animal_tag/group/admin.blade.php
@@ -16,23 +16,6 @@
     @enderror
 </div>
 
-<div class="form-group col-md-3 single" style="display: none">
-    <label for="name">Tag no <span class="t_r">*</span></label>
-    <select name="animal_info_id" id="animal_info"
-        class="form-control valReset @error('animal_info_id') is-invalid @enderror select2 singleInput"></select>
-    @error('animal_info_id')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-</div>
-
-<div class="form-group col-md-3 single" style="display: none">
-    <label for="tattoo_no">Tattoo no <span class="t_r">*</span></label>
-    <select name="tattoo_no" id="tattooNo"
-        class="form-control valReset @error('tattoo_no') is-invalid @enderror select2 singleInput"></select>
-    @error('tattoo_no')
-        <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-</div>
 
 <div class="form-group col-md-3 group" style="display: none">
     <label for="community_cat_id">Area <span class="t_r">*</span></label>
@@ -56,57 +39,77 @@
     <select name="community_id" id="subFarm" class="form-control valReset select2"></select>
 </div>
 
+
+<div class="form-group col-md-3 single" style="display: none">
+    <label for="name">Tag no <span class="t_r">*</span></label>
+    <select name="animal_info_id" id="animal_info"
+        class="form-control valReset @error('animal_info_id') is-invalid @enderror select2 singleInput"></select>
+    @error('animal_info_id')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+</div>
+
+<div class="form-group col-md-3 single" style="display: none">
+    <label for="tattoo_no">Tattoo no <span class="t_r">*</span></label>
+    <select name="tattoo_no" id="tattooNo"
+        class="form-control valReset @error('tattoo_no') is-invalid @enderror select2 singleInput"></select>
+    @error('tattoo_no')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+</div>
+
 @push('custom_scripts')
     <script>
         $(document).ready(function() {
+            let vac_type = $('input[name="vac_type"]:checked').val();
             $("#single").on("click", function() {
                 $(".single").show();
-                $("#single_input").attr("disabled", false);
-                $("#single_input").attr("required", true);
                 $("#to, #from").attr("disabled", true);
                 $(".group").hide();
                 $(".singleInput").attr('disabled', false);
                 $(".groupInput").attr('disabled', true);
                 let farm = $("#farm").val()
                 $("form").on('submit', function(e) {
-                    let farm = $("#farm").val()
                     let animal_info = $("#animal_info").val()
                     let tattooNo = $("#tattooNo").val()
-                    if (farm == null || farm == "") {
-                        Swal.fire(
-                            'Data Missing?',
-                            'Area Missing',
-                            'question'
-                        )
-                        return false;
+                    if (vac_type == 'single') {
+                        alert(single)
+                        if (farm == null || farm == "") {
+                            Swal.fire(
+                                'Data Missing?',
+                                'Area Missing',
+                                'question'
+                            )
+                            return false;
+                        }
+                        if ((animal_info == null || animal_info == 0) && (tattooNo == null ||
+                                tattooNo == 0)) {
+                            Swal.fire(
+                                'Data Missing?',
+                                'Tag No or Tattoo No Missing',
+                                'question'
+                            )
+                            return false;
+                        }
                     }
-                    if ((animal_info == null || animal_info == 0) && (tattooNo == null ||
-                            tattooNo == 0)) {
-                        Swal.fire(
-                            'Data Missing?',
-                            'Tag No or Tattoo No Missing',
-                            'question'
-                        )
-                        return false;
-                    }
+
                 });
             })
 
             $("#group").on("click", function() {
                 $(".single").hide();
-                $("#single_input").attr("disabled", true);
                 $("#to, #from").attr("disabled", false);
                 $("#to, #from").attr("required", true);
                 $(".group").show();
                 $(".singleInput").attr('disabled', true);
                 $(".groupInput").attr('disabled', false);
                 $(".total_vaccinated").show();
-                let farm = $("#farm").val()
                 $("form").on('submit', function(e) {
                     let farm = $("#farmGroup").val()
                     let animal_info = $("#animal_info").val()
                     let tattooNo = $("#tattooNo").val()
-                    if (farm == null || farm == "") {
+                    if (vac_type == 'group' && (farm == null || farm == "")) {
+                        alert(farm)
                         Swal.fire(
                             'Data Missing?',
                             'Area Missing',
@@ -192,41 +195,6 @@
                     })
                 }
             })
-
-            // $('#animal_info').on('change', function(e) {
-            //     var animalInfoId = $(this).val();
-            //     $.ajax({
-            //         url: '{{ route('get.getAnimalInfo') }}',
-            //         type: "get",
-            //         data: {
-            //             animalInfoId: animalInfoId
-            //         },
-            //         success: function(res) {
-            //             res = $.parseJSON(res);
-            //             $('#sex').val(res.sex);
-            //             $('#animal_sl').val(res.animal_sl);
-            //             $('#birth_wt').val(res.birth_wt);
-            //             $('#color').val(res.color);
-            //         }
-            //     })
-            // });
-            // $('#tattooNo').on('change', function(e) {
-            //     var animalInfoId = $(this).val();
-            //     $.ajax({
-            //         url: '{{ route('get.getAnimalInfo') }}',
-            //         type: "get",
-            //         data: {
-            //             animalInfoId: animalInfoId
-            //         },
-            //         success: function(res) {
-            //             res = $.parseJSON(res);
-            //             $('#sex').val(res.sex);
-            //             $('#animal_sl').val(res.animal_sl);
-            //             $('#birth_wt').val(res.birth_wt);
-            //             $('#color').val(res.color);
-            //         }
-            //     })
-            // });
         })
     </script>
 @endpush
