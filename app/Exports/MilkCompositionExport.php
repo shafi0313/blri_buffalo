9154d2c451168f9461514fa1a924df334cb84e0a
shafi1997 <msh.shafiul@gmail.com>
2023-08-09 20:16:15 +0600

    Fix: MIlk composition

--- app/Exports/MilkCompositionExport.php
+++ app/Exports/MilkCompositionExport.php
@@ -8,18 +8,18 @@ use Maatwebsite\Excel\Concerns\FromView;
 
 class MilkCompositionExport implements FromView
 {
-    // public $farm;
-    // public $farm_id;
-    // public function __construct($farm, $farm_id)
-    // {
-    //     $this->farm    = $farm;
-    //     $this->farm_id = $farm_id;
-    // }
+    public $farm;
+    public $farm_id;
+    public function __construct($farm, $farm_id)
+    {
+        $this->farm    = $farm;
+        $this->farm_id = $farm_id;
+    }
     public function view(): View
     {
         if (auth()->user()->permission == 1) {
             return view('admin.milk_composition.excel', [
-                'milkCompositions' => MilkComposition::all()
+                'milkCompositions' => MilkComposition::where($this->farm, '=', $this->farm_id)->get()
             ]);
         } else {
             return view('admin.milk_composition.excel', [
